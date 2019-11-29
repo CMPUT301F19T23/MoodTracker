bf1a2eb74cdeed952a89489e9a481c6bdff33b08
JiawangLin <jiawanglin11@gmail.com>
2019-11-28 20:10:26 -0700

    update test case1

---
+++ app/src/test/java/com/example/moodtracker/UserTest.java
@@ -0,0 +1,45 @@
+package com.example.moodtracker;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class UserTest {
+
+
+    private User mockUser() {
+        User user = new User("testName", "test@UA.ca");
+        return user;
+    }
+
+
+    @Test
+    void testGetUsername() {
+        User user = mockUser();
+
+        assertEquals("testName", user.getUsername());
+
+    }
+
+    @Test
+    void testSetUsername() {
+        User user = mockUser();
+        user.setUsername("anotherName");
+        assertEquals("anotherName", user.getUsername());
+    }
+
+    @Test
+    void testGetEmail() {
+        User user = mockUser();
+
+        assertEquals("test@UA.ca", user.getEmail());
+    }
+
+    @Test
+    void testSetEmail() {
+        User user = mockUser();
+        user.setEmail("new@UA.ca");
+        assertEquals("new@UA.ca", user.getEmail());
+    }
+}
